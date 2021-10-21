69b1214da6c3d0f931adff1e65be62302fe98033
= <alvarodediosgarcia1986@gmail.com>
2021-10-21 18:12:13 +0200

    TDD for buttons and show rigth valueright

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-    <div
-      :key="index"
-      v-for="(swatch, index) in swatches"
-      class="swatch"
-      :style="{ background: `#${swatch}` }"
-      :class="{ active : index === swatchActive }"
-      @click="swatchActive = index"
-    />
+    <div class="swatch__content">
+      <div
+        :key="`swatch-${index}`"
+        v-for="(swatch, index) in swatches"
+        class="swatch"
+        :style="{ background: `#${swatch}` }"
+        :class="{ active : index === swatchActive }"
+        @click="swatchActive = index"
+      />
+    </div>
+    <div class="btn__content">
+      <button
+        :key="`btn-${index}`"
+        v-for="(btn, index) in types"
+        class="btn"
+        :id="`btn-${btn}`"
+        :class="{ 'btn--active' : index === buttonActive}"
+        @click="buttonActive = index">
+      </button>
+    </div>
+    <div class="display__content">
+      <div class="display">
+        {{activeColorCode}}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,13 +32,20 @@
 export default {
   data() {
     return {
-      swatchActive: 0
+      types: ['hex', 'rgb', 'hsl'],
+      swatchActive: 0,
+      buttonActive: 0
     }
   },
   props: {
     swatches: {
       type: Array,
       default: []
+    }
+  },
+  computed:{
+    activeColorCode() {
+      return `#${this.swatches[this.swatchActive]}`
     }
   }
 }
--- tests/unit/color-picker.spec.js
+++ tests/unit/color-picker.spec.js
@@ -3,12 +3,20 @@ import ColorPicker from '@/components/ColorPicker.vue'
 import convert from 'color-convert'
 
 let wrapper = null // The Component
+const types =['hex', 'rgb', 'hsl']
 const propsData = {
   swatches: ['e3342f', '3490dc', 'f6993f', '38c172', 'fff']
 } // The props
 
 beforeEach(() => (
-  wrapper = shallowMount(ColorPicker, { propsData })
+  wrapper = shallowMount(ColorPicker, { 
+    data(){
+      return {
+        types
+      }
+    },
+    propsData 
+  })
 ))
 afterEach(() => wrapper.destroy())
 
@@ -31,5 +39,29 @@ describe('ColorPicker.vue', () => {
     const swatch = wrapper.findAll('.swatch').at(2)
     await swatch.trigger('click')
     expect(swatch.classes()).toContain('active')
+  })
+
+  test('Display a button for each three modes', () => {
+    const buttons = wrapper.findAll('.btn')
+    expect(buttons.length).toBe(3)
+    buttons.wrappers.forEach((button, index) => {
+      expect(button.element.id).toBe(`btn-${types[index]}`)
+    })
+  })
+
+  test('First button by default is active', () => {
+    const button = wrapper.find('.btn')
+    expect(button.classes()).toContain('btn--active')
+  })
+
+  test('Switch active button type onClick', async () => {
+    const button = wrapper.findAll('.btn').at(2)
+    await button.trigger('click')
+    expect(button.classes()).toContain('btn--active')
+  })
+
+  test('Display by default the first color code', () => {
+    const display = wrapper.find('.display')
+    expect(display.text()).toBe(`#${propsData.swatches[0]}`)
   })
 })
